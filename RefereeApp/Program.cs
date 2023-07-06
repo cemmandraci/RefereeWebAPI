d4e070cd4c4142c3724669fa48f2ab929f5b533e
Cem Mandraci <Cem.Mandraci@bs.nttdata.com>
2023-07-06 16:53:01 +0300

    Club controller and scoped added.

--- RefereeApp/Program.cs
+++ RefereeApp/Program.cs
@@ -22,6 +22,7 @@ builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
 builder.Services.AddScoped<IRefereeService, RefereeService>();
 builder.Services.AddScoped<IFixtureService, FixtureService>();
+builder.Services.AddScoped<IClubService, ClubService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
