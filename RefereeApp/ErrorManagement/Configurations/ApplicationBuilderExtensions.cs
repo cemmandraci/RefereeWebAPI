b042474447c544cbc145df7bfbd780fdb261229a
Cem Mandraci <Cem.Mandraci@bs.nttdata.com>
2023-07-17 22:56:03 +0300

    Added middleware configurations.

---
+++ RefereeApp/ErrorManagement/Configurations/ApplicationBuilderExtensions.cs
@@ -0,0 +1,9 @@
+﻿using RefereeApp.ErrorManagement.Configurations;
+
+namespace RefereeApp.Configurations;
+
+public static class ApplicationBuilderExtensions
+{
+    public static IApplicationBuilder AddGlobalErrorHandler(this IApplicationBuilder applicationBuilder) =>
+        applicationBuilder.UseMiddleware<GlobalErrorHandlingMiddleware>();
+}
